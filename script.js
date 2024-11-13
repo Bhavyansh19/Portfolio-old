da15292e16937ca9f5ab7748f8e807fc21fc6629
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2024-11-14 00:18:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Button for viewing CV
+const viewCvBtn = document.getElementById('viewCvBtn');
 viewCvBtn.addEventListener('click', () => {
     window.open('https://drive.google.com/file/d/1c6VZbXvTthSK9eJWDRyINoZugMgSnIVr/view?usp=sharing', '_blank');
+});
+
+// Button for contacting via email
+const contactMeBtn = document.getElementById('contactMeBtn');
+contactMeBtn.addEventListener('click', () => {
+    window.location.href = 'mailto:bhavyansh.jain@example.com?subject=Contacting%20You&body=Hello%20Bhavyansh,';
 });
