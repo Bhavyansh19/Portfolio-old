23b8a17818048aad901865ba6d7b278356651d43
Bhavyansh Jain <bhavyansh.bj@gmail.com>
2024-11-23 21:09:09 +0530

    responsive

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Button for viewing CV
 const viewCvBtn = document.getElementById('viewCvBtn');
-viewCvBtn.addEventListener('click', () => {
-    window.open('https://drive.google.com/file/d/1c6VZbXvTthSK9eJWDRyINoZugMgSnIVr/view?usp=sharing', '_blank');
-});
+if (viewCvBtn) {
+    viewCvBtn.addEventListener('click', () => {
+        window.open('https://drive.google.com/file/d/1c6VZbXvTthSK9eJWDRyINoZugMgSnIVr/view?usp=sharing', '_blank');
+    });
+}
 
 // Button for contacting via email
 const contactMeBtn = document.getElementById('contactMeBtn');
-contactMeBtn.addEventListener('click', () => {
-    window.location.href = 'mailto:bhavyansh.bj@gmail.com?subject=Contacting%20You&body=Hello%20Bhavyansh,';
-});
+if (contactMeBtn) {
+    contactMeBtn.addEventListener('click', () => {
+        window.location.href = 'mailto:bhavyansh.bj@gmail.com?subject=Contacting%20You&body=Hello%20Bhavyansh,';
+    });
+}
